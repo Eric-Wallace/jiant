d6a10838a4ef836b998e88280799efbe9fda555d
Eric Wallace <ewallac2@umd.edu>
2019-09-15 18:49:34 -0400

    upd scripts

--- eric_scripts/4-download_data_and_models.sh
+++ eric_scripts/4-download_data_and_models.sh
@@ -1,29 +1,32 @@
+RCLONE_DIR=~/bin/rclone
+
 #mkdir -p probing_data
-#/home/ericwallace/bin/rclone copy -v Eric:finetuning/probing_data/edges/ontonotes/const/ probing_data/edges/ontonotes/const/
+$RCLONE_DIR copy -v Eric:finetuning/probing_data/edges/ontonotes/const/ probing_data/edges/ontonotes/const/
 
 mkdir -p models
 
 mkdir -p models/rte
-/home/ericwallace/bin/rclone copy -v Eric:finetuning/models/rte models/rte
+$RCLONE_DIR copy -v Eric:finetuning/models/rte models/rte
 mkdir -p models/sts
-/home/ericwallace/bin/rclone copy -v Eric:finetuning/models/sts-b models/sts
+$RCLONE_DIR copy -v Eric:finetuning/models/sts-b models/sts
 #mkdir -p models/cola   # uses BERT-large right now
-#/home/ericwallace/bin/rclone copy -v Eric:finetuning/models/cola models/cola
+$RCLONE_DIR copy -v Eric:finetuning/models/cola models/cola
 mkdir -p models/sst
-/home/ericwallace/bin/rclone copy -v Eric:finetuning/models/sst models/sst
+$RCLONE_DIR copy -v Eric:finetuning/models/sst models/sst
 mkdir -p models/mrpc
-/home/ericwallace/bin/rclone copy -v Eric:finetuning/models/mrpc models/mrpc
+$RCLONE_DIR copy -v Eric:finetuning/models/mrpc models/mrpc
 mkdir -p models/mnli
-/home/ericwallace/bin/rclone copy -v Eric:finetuning/models/mnli models/mnli
+$RCLONE_DIR copy -v Eric:finetuning/models/mnli models/mnli
 mkdir -p models/coref
-/home/ericwallace/bin/rclone copy -v Eric:finetuning/models/allan-coref models/coref
+$RCLONE_DIR copy -v Eric:finetuning/models/allan-coref models/coref
 #mkdir -p models/agnews # in AllenNLP, need to extract pytorch-transformers part
-#/home/ericwallace/bin/rclone copy -v Eric:finetuning/models/agnews models/agnews
+#$RCLONE_DIR copy -v Eric:finetuning/models/agnews models/agnews
 mkdir -p models/memorization
-/home/ericwallace/bin/rclone copy -v Eric:finetuning/models/random-memorization models/memorization
+$RCLONE_DIR copy -v Eric:finetuning/models/random-memorization models/memorization
 mkdir -p models/multiqa
-/home/ericwallace/bin/rclone copy -v Eric:finetuning/models/5-way-multiqa models/multiqa
+$RCLONE_DIR copy -v Eric:finetuning/models/5-way-multiqa models/multiqa
 mkdir -p models/hotpot
-/home/ericwallace/bin/rclone copy -v Eric:finetuning/models/hotpot models/hotpot
+$RCLONE_DIR copy -v Eric:finetuning/models/hotpot models/hotpot
 mkdir -p models/squad
-/home/ericwallace/bin/rclone copy -v Eric:finetuning/models/squad models/squad
+$RCLONE_DIR copy -v Eric:finetuning/models/squad models/squad
+
